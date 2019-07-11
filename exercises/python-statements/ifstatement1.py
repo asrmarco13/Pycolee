e0a2f413b335a2d3e6f815dc321e880d63c824d3
Marco Orfei <morfei@iad2.it>
2019-07-11 14:38:37 +0200

    Update statements

--- exercises/python-statements/ifstatement1.py
+++ exercises/python-statements/ifstatement1.py
@@ -1,6 +1,6 @@
 def who_do_you_know():
     people_list = []
-    people_knows = input('Insert people names separated by comma (eg. ","): ') 
+    people_knows = input('Insert people names separated by comma (eg. ","): ')
     for person in (people_knows.split(',')):
         people_list.append(person.strip())
 
@@ -13,6 +13,7 @@ def ask_user(people_list):
         print('{} is present in the list'.format(person))
     else:
         print('{} isn\'t present in the list'.format(person))
+
 
 people_list = who_do_you_know()
 ask_user(people_list)
