4d0b0f00b22462faa455dbb16209fcf72416b32b
Marco Orfei <morfei@iad2.it>
2019-07-15 12:01:31 +0200

    Update data type exercise

--- exercises/data-types/sumstype.py
+++ exercises/data-types/sumstype.py
@@ -1,5 +1,6 @@
 def sum_method(arg1: int, arg2: int):
-    print(arg1 + arg2)
+    return arg1 + arg2
+
 
 def sum_method_args(*args: int) -> int:
     return sum(args)
@@ -7,6 +8,9 @@ def sum_method_args(*args: int) -> int:
 
 arg1 = 10
 arg2 = 20
-#sum_method(arg1, arg2)
-sum_return = sum_method_args(arg1, arg2)
-print(sum_return)
+result = sum_method(arg1, arg2)
+print('Result by sum arg1 + arg2:')
+print(result)
+result_from_args = sum_method_args(arg1, arg2)
+print('Result by invocation of sum method:')
+print(result_from_args)
