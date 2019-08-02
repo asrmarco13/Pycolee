bf593852087cd3e81b3033daf4139f6129c61bdb
Marco Orfei <morfei@iad2.it>
2019-08-02 08:22:56 +0200

    Update exercise

--- exercises/flask-wsgi/restful-sqlite-app/itemlist.py
+++ exercises/flask-wsgi/restful-sqlite-app/itemlist.py
@@ -1,9 +1,23 @@
+import sqlite3
 from flask_restful import Resource
-
-
-items = []
 
 
 class ItemList(Resource):
     def get(self):
+        connection = sqlite3.connect('shop.db')
+        cursor = connection.cursor()
+
+        query = 'SELECT * FROM items'
+        result = cursor.execute(query)
+        rows = result.fetchall()
+        connection.close()
+
+        items = []
+        for row in rows:
+            item = {
+                'name': row[0],
+                'price': row[1]
+            }
+            items.append(item)
+
         return {'items': items}
